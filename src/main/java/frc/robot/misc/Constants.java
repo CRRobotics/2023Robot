e923f82be70e027432b7f042b1c6181e7c86eb12
Kieran Stewart <kieran.stewart@icsd.k12.ny.us>
2023-01-20 19:03:14 -0500

    Implemented The DriveSubsytem from from MAXSwerve
    From https://github.com/REVrobotics/MAXSwerve-Java-Template

--- src/main/java/frc/robot/misc/Constants.java
+++ src/main/java/frc/robot/misc/Constants.java
@@ -1,6 +1,9 @@
 package frc.robot.misc;
 
 import com.revrobotics.CANSparkMax.IdleMode;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.util.Units;
 
 public interface Constants {
     interface DriveConstants {
@@ -31,5 +34,33 @@ public interface Constants {
         double turnEncoderPositionPIDMinInput = 0;
         IdleMode turnIdleMode = IdleMode.kBrake;
         int turnCurrentLimit = 50; // amps
+        //New constants that are new I guess (if they are 0 it is unset)
+        //Front Left Swerve
+        int kFrontLeftDrivingCanId = 0;
+        int kFrontLeftTurningCanId = 0;
+        double kFrontLeftChassisAngularOffset = 0;
+        //Front Right Swerve
+        int kFrontRightDrivingCanId = 0;
+        int kFrontRightTurningCanId = 0;
+        double kFrontRightChassisAngularOffset = 0;
+        //Rear Left Swerve
+        int kRearLeftDrivingCanId = 0;
+        int kRearLeftTurningCanId = 0;
+        double kBackLeftChassisAngularOffset = 0;
+        //Rear Right Swerve
+        int kRearRightDrivingCanId = 0;
+        int kRearRightTurningCanId = 0;
+        double kBackRightChassisAngularOffset = 0;
+        //SwerveDriveKinematics Object
+        public static final double kTrackWidth = Units.inchesToMeters(26.5);// Distance between centers of right and left wheels on robot//TODO Set kTrackWidth to actual track width
+        double kWheelBase = Units.inchesToMeters(26.5);// Distance between front and back wheels on robot //TODO Set kWheelBase to actual wheel base
+        SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
+                new Translation2d(kWheelBase / 2, kTrackWidth / 2),
+                new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
+                new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
+                new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));        //Swerve Max Speed (copied from https://github.com/REVrobotics/MAXSwerve-Java-Template/blob/main/src/main/java/frc/robot/Constants.java)
+        double kMaxSpeedMetersPerSecond = 3;
+        double kMaxAngularSpeed = 2 * Math.PI; // radians per second;
+        boolean kGyroReversed = false; //Determines whether the gyro is reversed (I think)
     }
 }
