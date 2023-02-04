22a2e6e5a601f954bcd8466e4401be1d7638c07e
TildenSmith <64168571+TildenSmith@users.noreply.github.com>
2023-02-04 09:30:40 -0500

    Elevator actually builds now

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,15 +1,25 @@
+package frc.robot.subsystems;
+
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import edu.upi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.misc.Constants;
+import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 public class Elevator extends SubsystemBase{
 
+    private VictorSPX armMotor;
     private final PIDController elevatorPID;
+    private final PIDController armPID;
     private CANSparkMax elevatorMotor;
     private RelativeEncoder elevatorEncoder;
-    public Elevator(int motorID){
+    public Elevator(int motorID, int motorID2){
         elevatorMotor = new CANSparkMax(motorID, MotorType.kBrushless);
+        armMotor = new VictorSPX(motorID2);
         elevatorPID = new PIDController(Constants.Elevator.elevatorP, Constants.Elevator.elevatorI, Constants.Elevator.elevatorD);
+        armPID = new PIDController(0.001, 0, 0); //TODO make these constants
         elevatorEncoder = elevatorMotor.getEncoder();
     }
 
