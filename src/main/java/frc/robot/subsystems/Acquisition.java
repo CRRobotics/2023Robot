bea09ec2c49100f38baa98d4faaa3812f6e7884a
Ethan-h48 <122328136+Ethan-h48@users.noreply.github.com>
2023-02-11 18:30:29 +0000

    changed code and added methods

--- src/main/java/frc/robot/subsystems/Acquisition.java
+++ src/main/java/frc/robot/subsystems/Acquisition.java
@@ -1,17 +1,23 @@
 package frc.robot.subsystems;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import frc.robot.misc.Constants;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.misc.ControllerWrapper;
 public class Acquisition extends SubsystemBase
 {
-    private WPI_VictorSPX victorSPX1 = new WPI_VictorSPX(Constants.Acquisition.acquisitionMotor1);
-    private WPI_VictorSPX victorSPX2 = new WPI_VictorSPX(Constants.Acquisition.acquisitionMotor2);
-    public Acquisition(){
-        if(ControllerWrapper.l2.getLeftTriggerAxis() > 0.5)
-        {
-            victorSPX1.set(1);
-            victorSPX2.set(1);
-        }
+    private VictorSPX victorSPX1;
+    private VictorSPX victorSPX2;
+    public Acquisition()
+    {
+        victorSPX1 = new VictorSPX(Constants.Acquisition.acquisitionMotor1);
+        victorSPX2 = new VictorSPX(Constants.Acquisition.acquisitionMotor2);
+    }
+    public void moveAcq (double status)
+    {
+        victorSPX1.set(status);
+    }
+    public void spinAcq (double speed)
+    {
+        victorSPX2.set(speed);
     }
 }
