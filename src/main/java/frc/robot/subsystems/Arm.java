dcbba3402fa1328342e403257365dba6ae743a74
TildenSmith <64168571+TildenSmith@users.noreply.github.com>
2023-02-11 14:58:46 -0500

    arm simulation working (one joint)

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -20,41 +20,6 @@ public class Arm {
   /*basically creates a class that stores all the parameters as pivs
     with getters and setters
   */
-  public class JointConfig {
-    private final double mass;
-    private final double length;
-    private final double moi;
-    private final double cgRadius;
-    private final DCMotor motor;
-
-    public JointConfig(double mass, double length, double moi, double cgRadius, DCMotor motor) {
-        this.mass = mass;
-        this.length = length;
-        this.moi = moi;
-        this.cgRadius = cgRadius;
-        this.motor = motor;
-    }
-
-    public double mass() {
-        return mass;
-    }
-
-    public double length() {
-        return length;
-    }
-
-    public double moi() {
-        return moi;
-    }
-    
-    public double cgRadius() {
-        return cgRadius;
-    }
-
-    public DCMotor motor() {
-        return motor;
-    }
-  }
 
   private final JointConfig joint_1;
   private final JointConfig joint_2;
--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -4,9 +4,13 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.Vector;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.wpilibj.simulation.ElevatorSim;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import edu.wpi.first.wpilibj.simulation.SimDeviceSim;
 import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
@@ -21,6 +25,7 @@ import com.revrobotics.RelativeEncoder;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 public class Elevator extends SubsystemBase{
+    private Arm arm;
     private VictorSPX armMotor;
     private final PIDController elevatorPID;
     private final PIDController armPID;
@@ -30,27 +35,41 @@ public class Elevator extends SubsystemBase{
     private MechanismLigament2d wrist;
     private MechanismLigament2d wrist2;
     private MechanismLigament2d elevator;
+    private SingleJointedArmSim armSim;
+    private double volts;
     public Elevator(int motorID, int motorID2){
+        arm = new Arm(new JointConfig(1, 1, 1, 1, DCMotor.getFalcon500(1)), 
+        new JointConfig(1, 1, 1, 1, DCMotor.getFalcon500(1)));
         elevatorMotor = new CANSparkMax(motorID, MotorType.kBrushless);
         armMotor = new VictorSPX(motorID2);
         elevatorPID = new PIDController(Constants.Elevator.elevatorP, Constants.Elevator.elevatorI, Constants.Elevator.elevatorD);
         armPID = new PIDController(0.001, 0, 0); //TODO make these constants
         mechanism = new Mechanism2d(3,3);
         MechanismRoot2d root = mechanism.getRoot("root", 1, 1);
-        elevator = root.append(new MechanismLigament2d("elavator", 1, 30));
+        elevator = root.append(new MechanismLigament2d("elavator", 1, 0));
         wrist = elevator.append(new MechanismLigament2d("wrist", 1, 1));
         wrist2 = wrist.append(new MechanismLigament2d("wrist2", 1, 1));
         SmartDashboard.putData("Mechanism", mechanism);
         elevatorEncoder = elevatorMotor.getEncoder();
         
-        SingleJointedArmSim armSim = new SingleJointedArmSim(new DCMotor(10, 10, 10, 10, 10, 1), 10, 1, 2, 0, Math.PI, 5, true);
+        
+        armSim = new SingleJointedArmSim(DCMotor.getFalcon500(1),
+          20, 0.0666, 0.2, 0, Math.PI * 2, 5, 
+          true);
+
+        armSim.setState(new Matrix<>(VecBuilder.fill(Math.PI * 3/4, 0)));
+        SmartDashboard.putNumber("volts", volts);
     }
 
     @Override
     public void periodic() {
-        wrist.setAngle(wrist.getAngle() + 1);
-        wrist2.setAngle(wrist2.getAngle() - 1);
-        SmartDashboard.putData("Mechanism", mechanism);
+        volts = SmartDashboard.getNumber("volts", volts);
+        double i = arm.calculate(VecBuilder.fill(1, 0)).get(0, 0);
+        armSim.setInputVoltage(1.23);
+        armSim.update(0.02);
+        System.out.println(armSim.getAngleRads() * 180 / Math.PI);
+        wrist.setAngle(armSim.getAngleRads() * 180 / Math.PI);
+        wrist2.setAngle(wrist2.getAngle() + 1);
     }
 
     public void setElevatorVelocity(double velocity)
