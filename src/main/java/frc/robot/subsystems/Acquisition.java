6c3daaf2398a5c274b5b0bf520299fc8f6d31866
Ethan-h48 <122328136+Ethan-h48@users.noreply.github.com>
2023-02-13 22:12:42 +0000

    Created AcqControl

--- src/main/java/frc/robot/subsystems/Acquisition.java
+++ src/main/java/frc/robot/subsystems/Acquisition.java
@@ -1,23 +1,25 @@
 package frc.robot.subsystems;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import frc.robot.misc.Constants;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Acquisition extends SubsystemBase
 {
     private VictorSPX victorSPX1;
     private VictorSPX victorSPX2;
+    private ControlMode mode;
     public Acquisition()
     {
+        mode = ControlMode.Velocity;
         victorSPX1 = new VictorSPX(Constants.Acquisition.acquisitionMotor1);
         victorSPX2 = new VictorSPX(Constants.Acquisition.acquisitionMotor2);
     }
-    public void moveAcq (double status)
+    public void moveAcq (double elevate)
     {
-        victorSPX1.set(status);
+        victorSPX1.set(mode, elevate);
     }
     public void spinAcq (double speed)
     {
-        victorSPX2.set(speed);
+        victorSPX2.set(mode, speed);
     }
 }
