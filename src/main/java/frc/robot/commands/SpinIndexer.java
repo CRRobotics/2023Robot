a66962d1e414c092dcc733a8a486435c0775f0d4
Nathan Birkett <98059854+NathanBirkett@users.noreply.github.com>
2023-02-15 19:56:34 -0500

    ready to test spinning with visions

--- src/main/java/frc/robot/commands/SpinIndexer.java
+++ src/main/java/frc/robot/commands/SpinIndexer.java
@@ -1,21 +1,25 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.misc.JeVoisInterface;
 import frc.robot.subsystems.Indexer;
 
 public class SpinIndexer extends CommandBase {
     Indexer indexer;
     double angle;
+    PIDController indexerPID;
+    JeVoisInterface jeVoisInterface;
     public SpinIndexer(Indexer indexer, double angle) {
         this.indexer = indexer;
-        addRequirements(indexer);
-        
+        addRequirements(indexer); 
+        indexerPID = new PIDController(0.01, 0, 0);
+        jeVoisInterface = new JeVoisInterface();
     }
 
     @Override
     public void execute() {
-        indexer.setIndexerVelocity(1);
+        indexer.setIndexerVelocity(indexerPID.calculate(jeVoisInterface.getConeAngle(), angle));
     }
 
     @Override
--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.misc.Constants;
 
@@ -10,9 +11,11 @@ public class Indexer extends SubsystemBase {
     private final VictorSPX indexerMotor;
     private final PIDController indexerPID;
     private double currentAngle;
+    private XboxController controller;
 
     public Indexer(int indexerMotorID){
         indexerMotor = new VictorSPX(indexerMotorID);
+        controller = new XboxController(0);
         indexerPID = new PIDController(Constants.Indexer.indexerP, Constants.Indexer.indexerI, Constants.Indexer.indexerD);
     }
 
@@ -22,7 +25,7 @@ public class Indexer extends SubsystemBase {
 
     public void setIndexerVelocity(double velocity){
         System.out.println(velocity);
-        indexerMotor.set(ControlMode.PercentOutput, velocity);
+        indexerMotor.set(ControlMode.PercentOutput, controller.getLeftX() * .5);
     }
 
     public double getAngle(){
