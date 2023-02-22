df289075d45f2fc7097b019e0947ee09ff46209e
ColbyEB <colby.bittner@icsd.k12.ny.us>
2023-02-22 14:42:00 -0500

    Update Elevator.java

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -2,21 +2,25 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.misc.Constants;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxLimitSwitch;
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 public class Elevator extends SubsystemBase{
-    private VictorSPX armMotor1;
-    private VictorSPX armMotor2;
+    private TalonFX armMotor1;
+    private TalonFX armMotor2;
     private final PIDController elevatorPID;
     private final PIDController armPID1;
     private final PIDController armPID2;
     private CANSparkMax elevatorMotor;
     private boolean coneOrCube;
+    private SparkMaxLimitSwitch bottomSwitch;
     //True for cone, false for cube
     private RelativeEncoder elevatorEncoder;
     private ControlMode control;
@@ -24,8 +28,8 @@ public class Elevator extends SubsystemBase{
         control = ControlMode.Position;
         coneOrCube = true;
         elevatorMotor = new CANSparkMax(motorID, MotorType.kBrushless);
-        armMotor1 = new VictorSPX(motorID2);
-        armMotor2 = new VictorSPX(motorID3);
+        armMotor1 = new TalonFX(motorID2);
+        armMotor2 = new TalonFX(motorID3);
         elevatorPID = new PIDController(Constants.Elevator.elevatorP, Constants.Elevator.elevatorI, Constants.Elevator.elevatorD);
         armPID1 = new PIDController(0.001, 0, 0); //TODO make these constants
         armPID2 = new PIDController(0.001, 0, 0); //TODO make these constants
