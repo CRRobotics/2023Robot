65856496a0d54ae34d6032564153dfe5357d490a
poodlemonarch <120506839+poodlemonarch@users.noreply.github.com>
2023-02-28 00:16:23 +0000

    Added documentation

--- src/main/java/frc/robot/subsystems/Acquisition.java
+++ src/main/java/frc/robot/subsystems/Acquisition.java
@@ -12,12 +12,21 @@ public class Acquisition extends SubsystemBase
     private PIDController acqPID;
     private boolean lowered;
 
+    /**
+     *Returns whether the acquisition is lowered
+     *@return True if the acquisition is lowered, false otherwise
+     */
     public boolean isLowered() {
         return lowered;
     }
+
+    /** 
+     * Sets lowered to input boolean
+     */
     public void setLowered(boolean lowered) {
         this.lowered = lowered;
     }
+
     public Acquisition()
     {
         mode = ControlMode.Velocity;
@@ -26,11 +35,21 @@ public class Acquisition extends SubsystemBase
         spinMotor = new VictorSPX(Constants.Acquisition.acquisitionMotor2);
         lowered = false;
     }
+
+    /**
+     * Moves acquisition
+     * @param elevate The distance to elevate
+     */
     public void moveAcq (double elevate)
     {
         // lowerMotor.set(ControlMode.Position, acqPID.calculate(lowerMotor.getClosedLoopError(), elevate));
     }
-    public void spinAcq (double speed)
+
+     /**
+     * Spins the acquisition
+     * @param speed The speed of the acquisition's rotation
+     */
+    public void spinAcq(double speed)
     {
         spinMotor.set(ControlMode.PercentOutput, speed);
     }
